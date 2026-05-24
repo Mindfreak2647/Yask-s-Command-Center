35d0f164d88247d8d970afd723ed9b6e08fe06c0
Mindfreak2647 <yaskupreti2647@gmail.com>
2026-05-25 01:21:34 +0530

    Update ClientController.js

--- widget code/ClientController.js
+++ widget code/ClientController.js
@@ -6,12 +6,57 @@ api.controller = function($interval, spModal) {
 
     c.priorityFilter = {};
 
+    c.currentTime = "";
+
+   
+
+    c.criticalServicesCount = 0;
+
+    c.regionalImpactCount = 0;
+
+
+    c.buildExecutiveMetrics = function() {
+
+        var services = {};
+
+        var regions = {};
+
+        c.data.incidents.forEach(function(inc) {
+
+
+            if(inc.priority == '1' &&
+               inc.business_service) {
+
+                services[inc.business_service] = true;
+
+            }
+
+            if(inc.location) {
+
+                regions[inc.location] = true;
+
+            }
+
+        });
+
+        c.criticalServicesCount =
+            Object.keys(services).length;
+
+        c.regionalImpactCount =
+            Object.keys(regions).length;
+
+    };
+
+
+    c.buildExecutiveMetrics();
+
 
     c.toggleExecutiveView = function() {
 
         c.executiveMode = !c.executiveMode;
 
     };
+
 
     c.filterPriority = function(priority) {
 
@@ -20,6 +65,7 @@ api.controller = function($interval, spModal) {
         };
 
     };
+
 
 
     c.clearFilter = function() {
@@ -33,60 +79,89 @@ api.controller = function($interval, spModal) {
 
         var now = new Date();
 
-        c.currentTime = now.toLocaleTimeString();
+        c.currentTime =
+            now.toLocaleTimeString();
 
     }
 
     updateClock();
 
-    $interval(updateClock, 1000);
+    var clockInterval =
+        $interval(updateClock, 1000);
 
-    $interval(function() {
 
-        c.server.refresh();
+    var refreshInterval = $interval(function() {
+
+        c.server.refresh().then(function() {
+
+            c.buildExecutiveMetrics();
+
+        });
 
     }, 20000);
 
+    c.$onDestroy = function() {
 
+        $interval.cancel(clockInterval);
+
+        $interval.cancel(refreshInterval);
+
+    };
     c.openIncident = function(inc) {
 
         spModal.open({
 
-            title: 'Incident Details - ' + inc.number,
+            title:
+                'Incident Details - ' +
+                inc.number,
 
             message:
 
-                '<div class=\"popup-content\">' +
+                '<div class="popup-content">' +
 
                 '<p><strong>Short Description:</strong> ' +
-                inc.short_description + '</p>' +
+                inc.short_description +
+                '</p>' +
 
                 '<p><strong>Priority:</strong> ' +
-                inc.priority + '</p>' +
+                inc.priority +
+                '</p>' +
 
                 '<p><strong>State:</strong> ' +
-                inc.state + '</p>' +
+                inc.state +
+                '</p>' +
 
                 '<p><strong>Assigned To:</strong> ' +
-                inc.assigned_to + '</p>' +
+                inc.assigned_to +
+                '</p>' +
 
                 '<p><strong>Assignment Group:</strong> ' +
-                inc.assignment_group + '</p>' +
+                inc.assignment_group +
+                '</p>' +
 
                 '<p><strong>Category:</strong> ' +
-                inc.category + '</p>' +
+                inc.category +
+                '</p>' +
 
                 '<p><strong>Business Service:</strong> ' +
-                inc.business_service + '</p>' +
+                inc.business_service +
+                '</p>' +
 
                 '<p><strong>Configuration Item:</strong> ' +
-                inc.cmdb_ci + '</p>' +
+                inc.cmdb_ci +
+                '</p>' +
+
+                '<p><strong>Location:</strong> ' +
+                (inc.location || 'N/A') +
+                '</p>' +
 
                 '<p><strong>Caller:</strong> ' +
-                inc.caller + '</p>' +
+                inc.caller +
+                '</p>' +
 
                 '<p><strong>Description:</strong><br>' +
-                (inc.description || 'No Description') +
+                (inc.description ||
+                 'No Description') +
                 '</p>' +
 
                 '</div>',
@@ -106,15 +181,114 @@ api.controller = function($interval, spModal) {
 
         var content = '';
 
-
         if(type === 'services') {
 
-            content =
-                '<ul>' +
-                '<li>Payment Gateway Service</li>' +
-                '<li>Customer Login API</li>' +
-                '<li>Core Banking Database</li>' +
-                '</ul>';
+            var services = {};
+
+            c.data.incidents.forEach(function(inc) {
+
+                if(inc.priority == '1' &&
+                   inc.business_service) {
+
+                    services[
+                        inc.business_service
+                    ] = true;
+
+                }
+
+            });
+
+            content = '<ul>';
+
+            Object.keys(services)
+                .forEach(function(service) {
+
+                content +=
+                    '<li>' +
+                    service +
+                    '</li>';
+
+            });
+
+            if(Object.keys(services)
+               .length === 0) {
+
+                content +=
+                    '<li>No critical services impacted</li>';
+
+            }
+
+            content += '</ul>';
+
+        }
+
+        else if(type === 'sla') {
+
+            content = '<ul>';
+
+            c.data.incidents.forEach(function(inc) {
+
+                if(inc.priority == '1') {
+
+                    content +=
+                        '<li>' +
+                        inc.number +
+                        ' - ' +
+                        inc.short_description +
+                        '</li>';
+
+                }
+
+            });
+
+            if(c.data.p1Count === 0) {
+
+                content +=
+                    '<li>No active SLA exposure</li>';
+
+            }
+
+            content += '</ul>';
+
+        }
+
+        else if(type === 'regions') {
+
+            var regions = {};
+
+            c.data.incidents.forEach(function(inc) {
+
+                if(inc.location) {
+
+                    regions[
+                        inc.location
+                    ] = true;
+
+                }
+
+            });
+
+            content = '<ul>';
+
+            Object.keys(regions)
+                .forEach(function(region) {
+
+                content +=
+                    '<li>' +
+                    region +
+                    '</li>';
+
+            });
+
+            if(Object.keys(regions)
+               .length === 0) {
+
+                content +=
+                    '<li>No regional impact detected</li>';
+
+            }
+
+            content += '</ul>';
 
         }
 
@@ -122,46 +296,27 @@ api.controller = function($interval, spModal) {
 
             content =
                 '<ul>' +
-                '<li>Online transactions impacted</li>' +
-                '<li>Estimated revenue exposure: $120K/hour</li>' +
-                '<li>Checkout failures increasing</li>' +
+
+                '<li>Active P1 Incidents: ' +
+                c.data.p1Count +
+                '</li>' +
+
+                '<li>Total Active Incidents: ' +
+                c.data.totalIncidents +
+                '</li>' +
+
+                '<li>Potential business disruption detected</li>' +
+
                 '</ul>';
 
         }
-
-        else if(type === 'sla') {
-
-            content =
-                '<ul>' +
-                '<li>' + c.data.p1Count +
-                ' Priority 1 incidents near SLA breach</li>' +
-
-                '<li>Database latency exceeding threshold</li>' +
-
-                '<li>Escalation triggered to operations team</li>' +
-                '</ul>';
-
-        }
-
-
-        else if(type === 'regions') {
-
-            content =
-                '<ul>' +
-                '<li>APAC region experiencing latency</li>' +
-                '<li>EU users reporting login failures</li>' +
-                '<li>North America stable</li>' +
-                '</ul>';
-
-        }
-
-
         spModal.open({
 
-            title: 'Executive Impact Analysis',
+            title:
+                'Executive Impact Analysis',
 
             message:
-                '<div class=\"popup-content\">' +
+                '<div class="popup-content">' +
                 content +
                 '</div>',
 
